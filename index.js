eba4ef6c4edc1b94ab186b612520381059314190
wwwal <w_larabi@yahoo.com>
2025-02-06 12:01:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./models');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/static`));
+
+app.get('/api/games', (req, res) => db.Game.findAll()
+  .then(games => res.send(games))
+  .catch((err) => {
+    console.log('There was an error querying games', JSON.stringify(err));
+    return res.send(err);
+  }));
+
+app.post('/api/games', (req, res) => {
+  const { publisherId, name, platform, storeId, bundleId, appVersion, isPublished } = req.body;
+  return db.Game.create({ publisherId, name, platform, storeId, bundleId, appVersion, isPublished })
+    .then(game => res.send(game))
+    .catch((err) => {
+      console.log('***There was an error creating a game', JSON.stringify(err));
+      return res.status(400).send(err);
+    });
+});
+
+app.delete('/api/games/:id', (req, res) => {
+  // eslint-disable-next-line radix
+  const id = parseInt(req.params.id);
+  return db.Game.findByPk(id)
+    .then(game => game.destroy({ force: true }))
+    .then(() => res.send({ id }))
+    .catch((err) => {
+      console.log('***Error deleting game', JSON.stringify(err));
+      res.status(400).send(err);
+    });
+});
+
+app.put('/api/games/:id', (req, res) => {
+  // eslint-disable-next-line radix
+  const id = parseInt(req.params.id);
+  return db.Game.findByPk(id)
+    .then((game) => {
+      const { publisherId, name, platform, storeId, bundleId, appVersion, isPublished } = req.body;
+      return game.update({ publisherId, name, platform, storeId, bundleId, appVersion, isPublished })
+        .then(() => res.send(game))
+        .catch((err) => {
+          console.log('***Error updating game', JSON.stringify(err));
+          res.status(400).send(err);
+        });
+    });
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
+
+module.exports = app;
